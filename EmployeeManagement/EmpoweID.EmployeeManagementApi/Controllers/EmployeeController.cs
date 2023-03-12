3ea4a501ab1699337cdd3a4a598d4c4ca214c74c
Mizan <mizanur.rahman@selise.ch>
2023-03-12 23:33:30 +0600

    Controller action added

--- EmployeeManagement/EmpoweID.EmployeeManagementApi/Controllers/EmployeeController.cs
+++ EmployeeManagement/EmpoweID.EmployeeManagementApi/Controllers/EmployeeController.cs
@@ -12,19 +12,23 @@ namespace EmpoweID.EmployeeManagementApi.Controllers
         private readonly IQueryHandler<SearchEmployeeQuery, SearchEmployeeQueryResult> _searchEmployeeQueryHandler;
         private readonly ICommandHandler<AddEmployeeRequest, AddEmployeeResult> _addEmployeeCommandHandler;
         private readonly ICommandHandler<UpdateEmployeeRequest, CommandResult> _updateEmployeeCommandHandler;
+        private readonly ICommandHandler<DeleteEmployeeRequest, CommandResult> _deleteEmployeeCommandHandler;
 
         public EmployeeController(IQueryHandler<GetEmployeeByIdQuery, GetEmployeeByIdQueryResult> getEmployeeByIdQueryHandler,
             IQueryHandler<SearchEmployeeQuery, SearchEmployeeQueryResult> searchEmployeeQueryHandler,
             ICommandHandler<AddEmployeeRequest, AddEmployeeResult> addEmployeeCommandHandler,
-            ICommandHandler<UpdateEmployeeRequest, CommandResult> updateEmployeeCommandHandler)
+            ICommandHandler<UpdateEmployeeRequest, CommandResult> updateEmployeeCommandHandler,
+            ICommandHandler<DeleteEmployeeRequest, CommandResult> deleteEmployeeCommandHandler)
         {
             this._getEmployeeByIdQueryHandler = getEmployeeByIdQueryHandler;
             this._searchEmployeeQueryHandler = searchEmployeeQueryHandler;
             this._addEmployeeCommandHandler = addEmployeeCommandHandler;
             this._updateEmployeeCommandHandler = updateEmployeeCommandHandler;
+            this._deleteEmployeeCommandHandler = deleteEmployeeCommandHandler;
         }
 
         [HttpGet]
+        [Route("{id:guid}")]
         public async Task<IActionResult> GetById([FromQuery] GetEmployeeByIdQuery query)
         {
             var employeeResult = await this._getEmployeeByIdQueryHandler.GetDataAsync(query);
@@ -37,13 +41,15 @@ namespace EmpoweID.EmployeeManagementApi.Controllers
             return Ok(employeeResult);
         }
 
+        
         [HttpGet]
+        [Route("search")]
         public async Task<IActionResult> searchEmployees([FromQuery] SearchEmployeeQuery query)
         {
             var employeeResult = await this._searchEmployeeQueryHandler.GetDataAsync(query);
             return Ok(employeeResult);
         }
-
+        
         [HttpPost]
         public async Task<IActionResult> AddEmployee([FromBody] AddEmployeeRequest request)
         {
@@ -51,6 +57,7 @@ namespace EmpoweID.EmployeeManagementApi.Controllers
             return Ok(response);
         }
 
+        
         [HttpPut]
         public async Task<IActionResult> UpdateEmployee([FromRoute] Guid id, [FromBody] UpdateEmployeeRequest request)
         {
@@ -58,13 +65,13 @@ namespace EmpoweID.EmployeeManagementApi.Controllers
             var response = await this._updateEmployeeCommandHandler.HandleAsync(request);
             return Ok(response);
         }
-
-        [HttpPut]
+        
+        [HttpDelete]
         public async Task<IActionResult> DeleteEmployee([FromRoute] Guid id, [FromBody] UpdateEmployeeRequest request)
         {
 
             var response = await this._updateEmployeeCommandHandler.HandleAsync(request);
             return Ok(response);
-        }
+        }                
     }
 }
---
+++ EmployeeManagement/EmpoweID.EmployeeManagementApi/Registrations.cs
@@ -0,0 +1,19 @@
+﻿using EmpoweID.EmployeeManagement.Core.Infrastructure;
+using EmpoweID.EmployeeManagement.Core.Model;
+using EmpoweID.EmployeeManagement.Data.Repositories;
+
+namespace EmpoweID.EmployeeManagementApi
+{
+    public static class Registrations
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            services.AddScoped<IQueryHandler<GetEmployeeByIdQuery, GetEmployeeByIdQueryResult>>();
+            services.AddScoped<IQueryHandler<SearchEmployeeQuery, SearchEmployeeQueryResult>>();
+            services.AddScoped<ICommandHandler<AddEmployeeRequest, AddEmployeeResult>>();
+            services.AddScoped<ICommandHandler<UpdateEmployeeRequest, CommandResult>>();
+            services.AddScoped<ICommandHandler<DeleteEmployeeRequest, CommandResult>>();
+        }
+    }
+}
